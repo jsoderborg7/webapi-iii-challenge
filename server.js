1174c39e8d922386713331047b8d2cba07d2ff27
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-02 14:34:36 -0600

    set up index.js and server.js, fixed express errors throughout app

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
+const express = require('express');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+function logger(req, res, next) {
+  console.log(`${req.method} made on ${req.url} at [${new Date().toISOString()}]`);
+  next();
+};
+
+server.use(logger);
+server.use(express.json());
+
+server.use(postRouter);
+server.use(userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
-
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
