47f13f43eb119ca59c48739cd2a0eb18ad93bb23
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-03 11:09:32 -0600

    added dotenv, preparing to deploy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server');
 
-port = 8000;
+const port = process.env.PORT || 4000;
+
 server.listen(port, () =>{
-  console.log(`\n**Server running on port ${port}**\n`);
+  console.log(`\n**Server running on localhost:${port}**\n`);
 });
